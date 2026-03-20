48d1c6685c49b8495026d3d2cb56c14c6169bdd4
wangxl <wangty334752@gmail.com>
2026-03-20 22:26:28 +0800

    feat: 引入核心库，安装vue-router （路由）、 pinia （状态管理）、 axios （网络请求）

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
+import ElementPlus from 'element-plus'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus)
+app.mount('#app')
