37c69cf6633a9df82a375d5b9f2e759f6097015d
wangxl <wangty334752@gmail.com>
2026-04-28 19:11:01 +0800

    feat: 添加菜单导航栏

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,16 +3,27 @@
     <Sidebar />
     <a-layout class="main-layout">
       <Navbar />
+      <NavigationTabs @refresh="refreshView" />
       <a-layout-content class="app-content">
-        <router-view />
+        <router-view :key="`${route.fullPath}-${refreshKey}`" />
       </a-layout-content>
     </a-layout>
   </a-layout>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
 import Sidebar from './Sidebar.vue'
 import Navbar from './Navbar.vue'
+import NavigationTabs from './NavigationTabs.vue'
+
+const route = useRoute()
+const refreshKey = ref(0)
+
+function refreshView() {
+  refreshKey.value += 1
+}
 </script>
 
 <style scoped>
@@ -24,5 +35,9 @@ import Navbar from './Navbar.vue'
   min-width: 0;
   min-height: 100vh;
   margin-left: 220px;
+}
+
+.app-content {
+  min-height: calc(100vh - 96px);
 }
 </style>
